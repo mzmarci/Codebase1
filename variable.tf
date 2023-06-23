15ed6dc347c4a878094f2ed76a33646e55abdf29
mzmarci <mzmazy100@gmail.com>
2023-06-23 12:07:21 +0100

    updating

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "ec2_ami" {
   description = "This is a variable to manage ec2 ami type"
   type        = string
-  
+
 }
 
 variable "ec2_instance_type" {
@@ -18,6 +18,6 @@ variable "ec2_default_tags" {
   description = "This is a variable to manage ec2 default_tags"
   type        = map(any)
   default = {
-    "Name" = "Assignment2"
+    "Name" = "Marci"
   }
 }
